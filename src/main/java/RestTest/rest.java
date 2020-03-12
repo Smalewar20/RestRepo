15e16483b24d474e99dde9f57a5417ab3aead062
Sonu Malewar <smalewar@xpanxion.com>
2020-03-12 13:03:32 +0530

    second commit for trial

---
+++ src/main/java/RestTest/rest.java
@@ -0,0 +1,42 @@
+package RestTest;
+
+import org.testng.annotations.Test;
+
+public class rest {
+	
+ int a =10;
+ int b= 20;
+ int c ;
+ 
+ 
+ @Test
+ 
+ public void swipeTest()
+ {
+	b=b-a;  //10
+	
+
+	
+	System.out.println(b);  // b=10
+	
+	
+    a=a+b;
+    
+    System.out.println(a); // a=20
+	 
+	
+	
+	 
+ }
+ 
+ 
+ 
+
+
+	
+	// it is sample test to commit and push code
+	
+	
+        
+
+}
